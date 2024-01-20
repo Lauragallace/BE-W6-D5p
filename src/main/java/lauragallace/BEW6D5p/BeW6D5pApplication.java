4f77d370ccdf272cc7cdeaf224d170844551a757
Lauragallace <143189799+Lauragallace@users.noreply.github.com>
2024-01-20 11:10:52 +0100

    first commit

---
+++ src/main/java/lauragallace/BEW6D5p/BeW6D5pApplication.java
@@ -0,0 +1,13 @@
+package lauragallace.BEW6D5p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeW6D5pApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeW6D5pApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lauragallace/BEW6D5p/BeW6D5pApplicationTests.java
@@ -0,0 +1,13 @@
+package lauragallace.BEW6D5p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeW6D5pApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
